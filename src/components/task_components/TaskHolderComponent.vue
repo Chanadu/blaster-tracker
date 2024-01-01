a51f66193974a9fd34fa82727dce0fb12139be6e
Chanadu <chandu.peddada@gmail.com>
2024-01-01 14:29:59 -0600

    t4eeqwrqwewrwERTweret

--- src/components/task_components/TaskHolderComponent.vue
+++ src/components/task_components/TaskHolderComponent.vue
@@ -1,8 +1,38 @@
 <template>
-	<div class="size border"></div>
+	<div class="border rounded-borders q-px-md q-py-sm row">
+		<div class="column col">
+			<div class="row col-5 flex justify-center align no-wrap">
+				<p class="text-h6">{{ title }}</p>
+				<div class="col"></div>
+				<p class="text-body1">{{ description }}</p>
+			</div>
+			<div class="col"></div>
+			<div class="row justify-between text-subtitle2 items-center align">
+				<p v-if="createdDate">
+					Created: {{ createdDate?.toLocaleDateString() }}
+				</p>
+				<p v-else></p>
+				<p v-if="endDate">
+					Ends: {{ endDate?.toLocaleDateString() ?? "" }}
+				</p>
+				<p v-else></p>
+			</div>
+		</div>
+		<div class="column col-shrink q-px-md q-pb-sm flex justify-around">
+			<q-icon name="warning" size="sm"></q-icon>
+			<q-icon name="warning" size="sm"></q-icon>
+		</div>
+	</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps({
+	title: String,
+	createdDate: Date,
+	endDate: Date,
+	description: String,
+});
+</script>
 
 <style scoped lang="scss">
 .border {
@@ -11,5 +41,8 @@
 .size {
 	width: 16em;
 	height: 16em;
+}
+.align {
+	align-items: first baseline;
 }
 </style>
--- src/pages/TaskTrackerPage.vue
+++ src/pages/TaskTrackerPage.vue
@@ -1,22 +1,104 @@
 <template>
-	<q-page class="row q-pa-xl q-gutter-lg justify-center align-center">
-		<task-holder-component></task-holder-component>
-		<task-holder-component></task-holder-component>
-		<task-holder-component></task-holder-component>
-		<task-holder-component></task-holder-component>
-		<task-holder-component></task-holder-component>
-		<task-holder-component></task-holder-component>
-		<task-holder-component></task-holder-component>
-		<task-holder-component></task-holder-component>
-		<task-holder-component></task-holder-component>
+	<q-page class="q-pa-xl q-gutter-md">
+		<task-holder-component
+			title="Longer title idk what is about to happen"
+			:createdDate="new Date()"
+			:endDate="new Date(2018, 12, 12)"
+			description="Testing Description"
+		>
+		</task-holder-component>
+		<task-holder-component
+			title="Longer title idk what is about to happen"
+			:createdDate="new Date()"
+			:endDate="new Date(2018, 12, 12)"
+			description="Testing Description"
+		>
+		</task-holder-component>
+		<task-holder-component
+			title="Longer title idk what is about to happen"
+			:createdDate="new Date()"
+			:endDate="new Date(2018, 12, 12)"
+			description="Testing Description"
+		>
+		</task-holder-component>
+		<task-holder-component
+			title="Longer title idk what is about to happen"
+			:createdDate="new Date()"
+			:endDate="new Date(2018, 12, 12)"
+			description="Testing Description"
+		>
+		</task-holder-component>
+		<task-holder-component
+			title="Longer title idk what is about to happen"
+			:createdDate="new Date()"
+			:endDate="new Date(2018, 12, 12)"
+			description="Testing Description"
+		>
+		</task-holder-component>
+		<task-holder-component
+			title="Longer title idk what is about to happen"
+			:createdDate="new Date()"
+			:endDate="new Date(2018, 12, 12)"
+			description="Testing Description"
+		>
+		</task-holder-component>
+		<task-holder-component
+			title="Longer title idk what is about to happen"
+			:createdDate="new Date()"
+			:endDate="new Date(2018, 12, 12)"
+			description="Testing Description"
+		>
+		</task-holder-component>
+		<task-holder-component
+			title="Longer title idk what is about to happen"
+			:createdDate="new Date()"
+			:endDate="new Date(2018, 12, 12)"
+			description="Testing Description"
+		>
+		</task-holder-component>
+		<task-holder-component
+			title="Longer title idk what is about to happen"
+			:createdDate="new Date()"
+			:endDate="new Date(2018, 12, 12)"
+			description="Testing Description"
+		>
+		</task-holder-component>
+		<task-holder-component
+			title="Longer title idk what is about to happen"
+			:createdDate="new Date()"
+			:endDate="new Date(2018, 12, 12)"
+			description="Testing Description"
+		>
+		</task-holder-component>
+		<task-holder-component
+			title="Longer title idk what is about to happen"
+			:createdDate="new Date()"
+			:endDate="new Date(2018, 12, 12)"
+			description="Testing Description"
+		>
+		</task-holder-component>
+		<task-holder-component
+			title="Longer title idk what is about to happen"
+			:createdDate="new Date()"
+			:endDate="new Date(2018, 12, 12)"
+			description="Testing Description"
+		>
+		</task-holder-component>
+		<q-page-sticky position="bottom-right" :offset="[32, 32]">
+			<q-btn fab icon="add" color="accent" ripple />
+		</q-page-sticky>
 	</q-page>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+// import { ref } from "vue";
 import TaskHolderComponent from "src/components/task_components/TaskHolderComponent.vue";
 
-let tasks = ref([{}, {}]);
+// let tasks = ref([{}, {}]);
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.border {
+	border: 1px solid $primary;
+}
+</style>
